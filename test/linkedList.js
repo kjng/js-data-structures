f6881e61e78b37a21e157792667c0ca30b8088d9
kjng <pianistkevinjang@gmail.com>
2017-04-06 08:36:10 -0700

    Test for prototype methods

--- test/linkedList.js
+++ test/linkedList.js
@@ -7,4 +7,17 @@ describe('linkedList', function() {
   it('should be a function', function() {
     expect(linkedList).to.be.a('function');
   });
+  it('should have a getValue method on the prototype', function() {
+    expect(linkedList.prototype.getValue).to.exist;
+  });
+  it('should have a setNext method', function() {
+    expect(linkedList.prototype.setNext).to.exist;
+  });
+  it('should have a getNext method', function() {
+    expect(linkedList.prototype.getNext).to.exist;
+  });
+  it('should set the node value from the argument', function() {
+    var list = new linkedList(1);
+    expect(list.getValue()).to.equal(1);
+  });
 });
