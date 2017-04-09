8414c33997975176b7270d8cca7b69ee6f7cae70
Kevin Jang <pianistkevinjang@gmail.com>
2017-04-08 20:31:38 -0700

    Rename file and capitalize SinglyLinkedList

---
+++ singlyLinkedList.js
@@ -0,0 +1,58 @@
+export function SinglyLinkedList(value) {
+  this.length = 0;
+  this.head = null;
+}
+
+SinglyLinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value);
+  var currentNode = this.head;
+
+  // if no head
+  if (!this.head) {
+    this.head = newNode;
+    this.length++;
+    return newNode;
+  }
+
+  // if list already has nodes
+  while (currentNode.next) {
+    currentNode = currentNode.next;
+  }
+
+  currentNode.next = newNode;
+  this.length++;
+  return newNode;
+}
+
+SinglyLinkedList.prototype.addToHead = function(value) {
+  var newNode = new Node(value);
+
+  // if no head
+  if (!this.head) {
+    this.head = newNode;
+    return newNode;
+  }
+
+  // if there is a head, set the existing one to the next node of our new node
+  newNode.next = this.head;
+  this.head = newNode;
+  return newNode;
+}
+
+export function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+
+Node.prototype.getValue = function() {
+  return this.value;
+}
+
+Node.prototype.setNext = function(listEl) {
+  this.next = listEl;
+  return listEl;
+}
+
+Node.prototype.getNext = function() {
+  return this.next;
+}
