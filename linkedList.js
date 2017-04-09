fa706f5098ef9b1826b2ea1ffefb94f0a7e536f6
Kevin Jang <pianistkevinjang@gmail.com>
2017-04-08 20:29:50 -0700

    Refactor listElement to be Node and add a container singlyLinkedList

--- linkedList.js
+++ linkedList.js
@@ -1,17 +1,58 @@
-export default function listElement(value) {
+export function singlyLinkedList(value) {
+  this.length = 0;
+  this.head = null;
+}
+
+singlyLinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value);
+  var currentNode = this.head;
+
+  // if no head
+  if (!this.head) {
+    this.head = newNode;
+    this.length++;
+    return newNode;
+  }
+
+  // if list already has nodes
+  while (currentNode.next) {
+    currentNode = currentNode.next;
+  }
+
+  currentNode.next = newNode;
+  this.length++;
+  return newNode;
+}
+
+singlyLinkedList.prototype.addToHead = function(value) {
+  var newNode = new Node(value);
+
+  // if no head
+  if (!this.head) {
+    this.head = newNode;
+    return newNode;
+  }
+
+  // if there is a head, set the existing one to the next node of our new node
+  newNode.next = this.head;
+  this.head = newNode;
+  return newNode;
+}
+
+export function Node(value) {
   this.value = value;
   this.next = null;
 }
 
-listElement.prototype.getValue = function() {
+Node.prototype.getValue = function() {
   return this.value;
 }
 
-listElement.prototype.setNext = function(listEl) {
+Node.prototype.setNext = function(listEl) {
   this.next = listEl;
   return listEl;
 }
 
-listElement.prototype.getNext = function() {
+Node.prototype.getNext = function() {
   return this.next;
 }
